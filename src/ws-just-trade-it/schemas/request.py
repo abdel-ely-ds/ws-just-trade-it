7b14005625c05a06b8c64be676eec285e20748df
abdel-ely-ds <abdel.ely.ds@gmail.com>
2021-10-02 20:55:48 +0200

    using Field

--- src/ws-just-trade-it/schemas/request.py
+++ src/ws-just-trade-it/schemas/request.py
@@ -1,9 +1,12 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from pydantic.typing import DictStrAny
 
 
 class Request(BaseModel):
-    code: str
-    analysis_type: Optional[str] = "MACRO"
-    stock_name: Optional[str] = "msft"
+    code: str = Field(..., description="Your startegy implementation which should be a sub-class of Strategy")
+    analysis_type: Optional[str] = Field(
+        default="MACRO", description="Two availble types of analysis: MACRO & MICRO")
+    stock_name: Optional[str] = Field(
+        default="msft", description="The name of the stock to backtest")
