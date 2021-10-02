8c192c6c8247d6a156a1894e6aa7cffc56b5e4d5
abdel-ely-ds <abdel.ely.ds@gmail.com>
2021-10-02 21:17:36 +0200

    new exception added

--- src/ws-just-trade-it/exceptions/exceptions.py
+++ src/ws-just-trade-it/exceptions/exceptions.py
@@ -2,3 +2,9 @@ class StockNameDoesNotExist(Exception):
     def __init__(self, message: str) -> None:
         self.message = message
         super().__init__(self.message)
+
+
+class AnalysisTypeDoesNotExist(Exception):
+    def __init__(self, message: str) -> None:
+        self.message = message
+        super().__init__(self.message)
