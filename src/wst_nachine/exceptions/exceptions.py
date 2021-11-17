ff3a10e8222279ee6b3e8f1fe59063fd4d416651
abdel-ely-ds <76486913+abdel-ely-ds@users.noreply.github.com>
2021-11-18 00:48:26 +0100

    Update exceptions.py

--- src/wst_nachine/exceptions/exceptions.py
+++ src/wst_nachine/exceptions/exceptions.py
@@ -4,12 +4,6 @@ class StockNameDoesNotExist(Exception):
         super().__init__(self.message)
 
 
-class AnalysisTypeDoesNotExist(Exception):
-    def __init__(self, message: str) -> None:
-        self.message = message
-        super().__init__(self.message)
-
-
 class StrategyNotImplemented(Exception):
     def __init__(self, message: str) -> None:
         self.message = message
